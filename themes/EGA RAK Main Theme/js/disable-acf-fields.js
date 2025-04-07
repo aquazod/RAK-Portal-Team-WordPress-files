371cfe21e062617b39be5e4e0a14349bc8f9bccb
Samy Sorial <156057109+Samy-Sorial@users.noreply.github.com>
2025-04-07 11:05:15 +0200

    Update disable-acf-fields.js

--- themes/EGA RAK Main Theme/js/disable-acf-fields.js
+++ themes/EGA RAK Main Theme/js/disable-acf-fields.js
@@ -1,24 +1,18 @@
 jQuery(document).ready(function ($) {
-    console.log("🔧 ACF field disabling script loaded");
     $('.disabled-acf-field').each(function (index) {
         let $field = $(this);
-        console.log(`➡️ Disabling field #${index + 1}:`, $field);
-
         // Disable input elements
         $field.find('input, textarea, select, button').each(function () {
-            console.log("  🚫 Disabling input:", this);
             $(this).prop('disabled', true);
         });
 
         // Remove image/file/gallery edit buttons
         $field.find('.acf-button, .acf-actions a').each(function () {
-            console.log("  ❌ Removing ACF button or action:", this);
             $(this).remove();
         });
 
         // Disable image/file/gallery uploader interactions
         $field.find('.acf-image-uploader, .acf-file-uploader, .acf-gallery').each(function () {
-            console.log("  🧊 Disabling pointer events for uploader:", this);
             $(this).css('pointer-events', 'none');
         });
     });
