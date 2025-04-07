14d00a489b1aa7f6d753a6f628b78f09e66a599a
MinaEmadFakhry <m.labib@egac.rak.ae>
2025-04-07 11:32:17 +0200

    adding flags and banners and relocating the styles enqueuing

--- plugins/Hero-elementor/customElement.php
+++ plugins/Hero-elementor/customElement.php
@@ -9,8 +9,10 @@
 if (!defined('ABSPATH')) exit;
 
 // Register the custom Elementor widget 
-function register_hero_elementor_widget($widgets_manager) {
+function register_elementor_widgets($widgets_manager) {
     require_once(__DIR__ . '/widgets/class-hero-widget.php');
+    require_once(__DIR__ . '/widgets/class-banner-widget.php');
+    require_once(__DIR__ . '/widgets/class-flag-widget.php');
 
     // Fix: Ensure class name matches the actual class definition
     if (class_exists('Hero_Elementor_Widget')) {
@@ -18,7 +20,38 @@ function register_hero_elementor_widget($widgets_manager) {
     } else {
         error_log('Hero_Elementor_Widget class not found.');
     }
+    if (class_exists('Banner_Elementor_Widget')) {
+        $widgets_manager->register(new \Banner_Elementor_Widget());
+    } else {
+        error_log('Banner_Elementor_Widget class not found.');
+    }
+    if (class_exists('Flag_Elementor_Widget')) {
+        $widgets_manager->register(new \Flag_Elementor_Widget());
+    } else {
+        error_log('Flag_Elementor_Widget class not found.');
+    }
 }
 
-add_action('elementor/widgets/register', 'register_hero_elementor_widget');
+// enqueue the styles for the widgets
+function enqueue_widget_styles_global() {
+    wp_enqueue_style(
+        'hero-css',
+        plugins_url('css/hero-style.css', __FILE__),
+        [],
+        filemtime(plugin_dir_path(__FILE__) . 'css/hero-style.css'),
+        'all'
+    );
+
+    wp_enqueue_style(
+        'flag-css',
+        plugins_url('css/Flag-Styles.css', __FILE__),
+        [],
+        filemtime(plugin_dir_path(__FILE__) . 'css/Flag-Styles.css'),
+        'all'
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_widget_styles_global');
+
+
+add_action('elementor/widgets/register', 'register_elementor_widgets');
 
