1c5a08e20299cc96f662bf336351951e851e59e1
Youmna Yousry Mohamed Ibrahim <y.yousry@egac.rak.ae>
2025-04-08 10:42:40 +0200

    Adding Second Version Changes

--- themes/EGA RAK Main Theme/single-dynTooltip.php
+++ themes/EGA RAK Main Theme/single-dynTooltip.php
@@ -1,12 +1,11 @@
 <?php
-
-$tooltip_content = get_field('tooltip_content'); // Retrieve the tooltip content
-$trigger_type = get_field('trigger_type'); // Retrieve the trigger type
-$trigger_text = get_field('trigger_text'); // Retrieve the trigger text, if any
+$tooltip_content = get_field('tooltip_content');
+$trigger_type = get_field('trigger_type');
+$trigger_text = get_field('trigger_text');
+$trigger_image = get_field('trigger_image'); // New image field
 
 if ($tooltip_content):
     if ($trigger_type == 'icon'):
-        // Display an information icon as the trigger
         ?>
         <span class="tooltip-trigger">
             <i class="info-icon">i</i>
@@ -14,13 +13,19 @@ if ($tooltip_content):
         </span>
         <?php
     elseif ($trigger_type == 'text' && $trigger_text):
-        // Display custom text as the trigger
         ?>
         <span class="tooltip-trigger">
             <?php echo esc_html($trigger_text); ?>
             <span class="tooltip-content"><?php echo esc_html($tooltip_content); ?></span>
         </span>
         <?php
+    elseif ($trigger_type == 'image' && $trigger_image):
+        ?>
+        <span class="tooltip-trigger">
+            <img src="<?php echo esc_url($trigger_image); ?>" alt="tooltip image" style="max-width: 24px; cursor: pointer;">
+            <span class="tooltip-content"><?php echo esc_html($tooltip_content); ?></span>
+        </span>
+        <?php
     endif;
 endif;
- ?>
+?>
